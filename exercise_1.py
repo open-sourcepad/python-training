02b71acbdaa3a55cea2bdd2e40e1f1d85ff9b0ec
Erik John Moreno <erikjohnmoreno@Eriks-MacBook-Pro.local>
2021-02-19 16:06:07 +0800

    erikpogi the great

--- exercise_1.py
+++
@@ -1,9 +0,0 @@
-tups = ('fonsy', 5)
-sets = {'fonsy', '5'}
-lists = ['fonsy', 5]
-dictionaries = dict(name= 'fonsy', rice= 5)
-
-print("My name is {} and my Mang Inasal rice record is {}.".format(*tups))
-print("My name is {} and my Mang Inasal rice record is {}.".format(*sets))
-print("My name is {} and my Mang Inasal rice record is {}.".format(*lists))
-print("My name is {name} and my Mang Inasal rice record is {rice}.".format(**dictionaries))
---
+++ string.py
@@ -0,0 +1,12 @@
+name = "Erik pogi the great"
+rice_record = 5
+
+tuple = (name, rice_record,)
+list = [name, rice_record]
+set = {name, rice_record}
+dict = {'name': name, 'rice_record': rice_record}
+
+print(f"TUPLE: My name is {name} and my mang inasal rice record is: {rice_record}".format(name=tuple[0], rice_record=tuple[1]))
+print(f"LIST: My name is {name} and my mang inasal rice record is: {rice_record}".format(name=list[0], rice_record=list[1]))
+print(f"lol paano pag sets")
+print(f"DICT: My name is {name} and my mang inasal rice record is: {rice_record}".format(name=dict['name'], rice_record=dict['rice_record']))
