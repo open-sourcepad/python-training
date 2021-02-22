fe5a274e04ac8605bdb659bf27e542247874182f
John Norman Capule <johnnormancapule@gmail.com>
2021-02-22 15:44:59 +0800

    Minor refactors

--- exercise_2.py
+++ exercise_2.py
@@ -1,7 +1,6 @@
 def inasalize(tuple_input):
-  mang_inasal_valid = (1,1.5,2,3,4,5,6,7)
-  filtered_tuple = (inasal for inasal in tuple_input if inasal in mang_inasal_valid)
-  return list(map(lambda inasal: f"PM{inasal}", filtered_tuple))
+  mang_inasal_valid = (1,1.5,2,3,4,5,6,7,)
+  return list((f"PM {inasal}" for inasal in tuple_input if inasal in mang_inasal_valid))
 
-values = (1,1.5,2,2.5,3,3.7,4,5,6,7)
+values = (1,1.5,2,2.5,3,3.7,4,5,6,7,)
 print(inasalize(values))
