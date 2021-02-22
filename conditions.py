e1ff3f6344aaf113f3a992a883854172524ed3fd
Alfonso Juan Dillera <aj.dillera@gmail.com>
2021-02-22 15:31:48 +0800

    PM meal printout.

--- conditions.py
+++ conditions.py
@@ -1,3 +1,4 @@
 meals = (1, 1.5, 2, 2.5, 3, 3.7, 4, 5, 6, 7,)
+pm_meals = []
 
-[print("PM {}".format(meal)) for meal in meals if meal not in [2.5, 3.7]]
+[pm_meals.append("PM {}".format(meal)) for meal in meals if meal not in [2.5, 3.7]]
